e640266da93d21cd41b62cee8887716776a1704d
root <root@453a21327d7d>
2016-04-20 13:14:52 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "Prefab1",
+  "homePage" : "Main",
+  "name" : "prefab1",
+  "platformType" : "DEFAULT",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
